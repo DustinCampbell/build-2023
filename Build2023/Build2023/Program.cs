a8cce6685367159246ec913de08228b59ffe77cd
Dustin Campbell <dustin@teamcampbell.org>
2023-05-23 10:58:49 -0700

    Finished demo

---
+++ Build2023/Build2023/Program.cs
@@ -0,0 +1,21 @@
+﻿using Pair = (int First, int Second);
+using Grade = decimal;
+
+var mads = new Student("Mads Torgersen", 900751, [3.5m, 2.9m, 1.8m, .. Grades.Dustin]);
+
+WriteLine(mads.GPA);
+
+public class Student(string name, int id, Grade[] grades) : Person(name)
+{
+    public string Name { get; set => field = value.Trim(); } = name;
+    public int Id => id;
+
+    public Student(string name, int id) : this(name, id, []) { }
+
+    public decimal GPA => grades switch
+    {
+        [] => 4.0m,
+        [var grade] => grade,
+        [.. var all] => all.Average()
+    };
+}
